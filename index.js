b449eb09c054b52e0080985682529f01bb210376
Edinson <ev.samar.c@gmail.com>
2021-03-18 10:41:43 -0500

    Mensaje Bienvenida

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.use(express.json());
 //ROUTER
 app.use('/mail', mail);
 
+app.use('/', function(req, res){
+
+    res.status(200).send('<marquee>Bienvenido al servicio de mailing transac de UMC -  Revise la documentación para continuar</marquee>');
+
+});
+
 app.listen(config.api.port, ()=>{
     console.log('Server a la escucha -> ' , config.api.port , 'http://localhost:'+config.api.port );
 } );
