4c0c4fa5e9d57206dc69d2bc55f4eada9a4fc79a
Edinson <ev.samar.c@gmail.com>
2022-02-03 12:03:27 -0500

    Ultimo cambio 2022

--- components/mail/controller.js
+++ components/mail/controller.js
@@ -26,12 +26,7 @@ async function send( data )
 
     return info;
 
-    //   console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-    // Preview only available when sending through an Ethereal account
-    //   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
 // main().catch(console.error);
