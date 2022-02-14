cda4a42af46ed1cf32e8e68bd73d428ae09de49e
Edinson <ev.samar.c@gmail.com>
2022-02-14 17:17:43 -0500

    Cors añadido

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const config = require('./config.js');
 
@@ -6,7 +7,8 @@ const mail = require('./components/mail/network');
 
 const app = express();
 
-app.use(express.json());
+app.use(cors());
+// app.use(express.json());
 
 
 //ROUTER
